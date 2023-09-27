8b1f280e05aa16aee231721dd918a60466a62fae
Kronos Codeur <piraliarris@gmail.com>
2023-09-27 20:14:34 +0000

    auth config

--- migrations/Version20230927195821.php
+++ migrations/Version20230927195821.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230926101845 extends AbstractMigration
+final class Version20230927195821 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20230926101845 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE utilisateur (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(35) NOT NULL, password VARCHAR(10) NOT NULL)');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL)');
+        $this->addSql('DROP TABLE utilisateur');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE utilisateur');
+        $this->addSql('CREATE TABLE utilisateur (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(255) NOT NULL COLLATE "BINARY", email VARCHAR(35) NOT NULL COLLATE "BINARY", password VARCHAR(10) NOT NULL COLLATE "BINARY")');
+        $this->addSql('DROP TABLE user');
     }
 }
--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,25 +2,39 @@
 
 namespace App\Controller;
 
-use Psr\Container\ContainerInterface;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AuthenticationController extends AbstractController
 {
-    #[Route('/authentication', name: 'app_authentication')]
-    public function index(): Response
-    {
-        return $this->render('authentication/index.html.twig', [
-            'controller_name' => 'AuthenticationController',
-        ]);
-    }
-    public function login(): Response{
+    #[Route('/login', name: 'app_login')]
+    public function appLogin(): Response{
         return $this-> render('authentication/login.html.twig');
     }
-    public function register(): Response
+    #[Route('/login', name: 'app_register')]
+    public function appRegister(): Response
     {
         return  $this->render('authentication/register.html.twig');
     }
+
+    #[Route("/user/login",name: "login",methods: ['POST'])]
+    public function login(Request $request,UserRepository $userRepository)
+    {
+        $email = $request->get('email');
+        $password = $request->get('password');
+        $user=$userRepository->findBy(['email'=>$email,'password'=>$password]);
+        if ($user!=null){
+            return $this->render('home/index.html.twig',[
+                "user"=>$user
+            ]);
+        }else{
+            return $this->render('authentication/login.html.twig',[
+                "error"=>"invalid credentials"
+            ]);
+        }
+    }
 }
+
